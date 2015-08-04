a813ecd3994646e4c15407d7a1496b96a66ae49d
rcayabyab <cayabyab.ryan@gmail.com>
2015-08-03 20:21:01 -0400

    basic jquery commands

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,40 @@
-//= require_tree .
+//= require "_jquery"
+
+$(document).ready(function(){
+
+	$(function() { // SMOOTH SCROLL
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+	$('.hamburger').click(function(e) { // TOGGLE MOBILE NAV ON HAMBURGER CLICK
+	  e.preventDefault();
+	  $('header nav ul').toggleClass('show_nav');
+	  $(this).toggleClass('menuopen');
+	  //FORCES REDRAW IN MOBILE SAFARI
+	  if (navigator.userAgent.match(/Version\/[\d\.]+.*Safari/)) {
+	    $('header nav ul').hide();
+	    $('header nav ul').get(0).offsetHeight;
+	    $('header nav ul').show();
+	    $(this).hide();
+	    $(this).get(0).offsetHeight;
+	    $(this).show();
+	  }
+	});
+
+	$('header nav ul li a').click(function() { // TOGGLE MOBILE NAV ON NAV ITEM CLICK
+	  $('header nav ul').toggleClass('show_nav');
+	  $('.hamburger').toggleClass('menuopen');
+	});
+
+});
