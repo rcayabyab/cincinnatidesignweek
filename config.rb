f88a8e71407ce48538cc274411acec8b44e8e108
rcayabyab <cayabyab.ryan@gmail.com>
2015-08-14 13:23:49 -0400

    added minify html gem

--- config.rb
+++ config.rb
@@ -63,6 +63,9 @@ configure :build do
   # Minify Javascript on build
   activate :minify_javascript
 
+  # ACTIVATE MINIFY HTML
+  activate :minify_html
+
   # Enable cache buster
   # activate :asset_hash
 
